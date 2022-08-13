2354f50cccba8f33fb6b95b5724556ae451ca1fb
Ferhat Güneş <guneshoca1@gmail.com>
2022-08-13 23:16:25 +0300

    yeni v

---
+++ src/Day1Git.java
@@ -0,0 +1,10 @@
+public class Day1Git {
+    public static void main(String[] args) {
+        System.out.println("Hello Git 1. deneme");
+        System.out.println("Hello Git 2. deneme");
+        System.out.println("Hello Git 3. deneme");
+        System.out.println("Hello Git 4. deneme");
+        System.out.println("Hello Git 5. deneme");
+
+    }
+}
