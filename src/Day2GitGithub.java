b98aedf8999e77d53504ecc310a756c8f809e5e6
Ferhat Güneş <guneshoca1@gmail.com>
2022-08-15 21:02:32 +0300

    V2

---
+++ src/Day2GitGithub.java
@@ -0,0 +1,24 @@
+public class Day2GitGithub {
+
+    /*
+    1-  git init ---> local repo oluşturmak için kullanılır.
+    yani git ile klasörümüzün içerisindeki dosyaları ilişkilendirmek için kullanılır.
+
+    2-  git add .  ---> working spaceden (yani yerel) dosyalarımı stage area yani commitlemek için beklenen yere gönderir.
+
+    3-  git commit -m "mesaj" ---> stage areadan commit store dosyalarımı göndermek için kullanılır.
+    commit yani versiyon yani sürüm oluşturmuş olutur.
+
+    4- git push  ----> uzak repoya yani github'a göndermek için kullandığımız kod.
+    yanlız git push komutunu direk kullanmak istersek bir kez
+            git remote add origin -adres-
+            git push -u origin master
+
+
+     */
+
+
+
+}
+
+
